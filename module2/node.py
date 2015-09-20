e88a11a6abeb847cfeb54da75330e577baa56131
Fredrik Christoffer Berg <fredrcb@stud.ntnu.no>
2015-09-20 13:31:46 +0200

    working gui, module 2

--- module2/node.py
+++ module2/node.py
@@ -1,12 +1,20 @@
-def Node():
+class Node():
 	'''
 
 	'''
 
 
-	def __init__(cartesianX, cartesianY):
+	def __init__(self, index, cartesian_x, cartesian_y):
 		'''
 
 		'''
-		self.x = x
-		self.y = y
+		self.index = index
+		self.cartesian_x = cartesian_x
+		self.cartesian_y = cartesian_y
+		self.edges = list()
+
+	def __str__(self):
+		return str(self.index) + " - " + str(self.cartesian_x) + " - " + str(self.cartesian_y)
+
+	def __repr__(self):
+		return str(self.index) + " - " + str(self.cartesian_x) + " - " + str(self.cartesian_y)
