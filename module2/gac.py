14c82fbe32febb793c459eb60b2ffb8190c30991
talepre <tale.prestmo@gmail.com>
2015-10-03 11:28:06 +0200

    mod2 with makefunc in gac

--- module2/gac.py
+++ module2/gac.py
@@ -1,6 +1,4 @@
 class GAC:
-
-
 	'''
 
 	'''
@@ -12,33 +10,37 @@ class GAC:
 		self.domains = domains
 		self.revise_queue = list()
 		for node in self.constraints.involved:
-			for constraint in self.constraints.involved[node]:
-				self.revise_queue.append([node, constraint])
-		
-		
+			for constraint_edge in self.constraints.involved[node]:
+				self.revise_queue.append([node, constraint_edge])
 
 	def domain_filtering_loop(self):
 		'''
 
 		'''
 		while self.revise_queue:
-			node, constraint = self.revise_queue.pop(0)
-			if self.revise(node, constraint):
-				for constraint_edge in self.constraints.involved[constraint]:
-					if [constraint, constraint_edge] not in self.revise_queue:
-						self.revise_queue.append([constraint, constraint_edge])
+			node, constraint_node = self.revise_queue.pop(0)
+			if self.revise(node, constraint_node):
+				for edges in self.constraints.involved[node]:
+					if [edges, node] not in self.revise_queue:
+						self.revise_queue.append([edges, node])
+
 		return self.domains
 
-	def revise(self, node, constraint):
+	def revise(self, node, constraint_node):
 		'''
+		node repr med tall 0, constraint repr som lambdafunksjon.
+		'''
+		revised = False
+		for x_domain in self.domains[node]:
+			satisfies = 0
+			for y_domain in self.domains[constraint_node]:
+				if self.constraints.expression(x_domain, y_domain):
+					satisfies += 1
+			if satisfies == 0:
+				self.domains[node].remove(x_domain)
+				revised = True
 
-		'''
-		if len(self.domains[constraint]) == 1:
-			#Constraints.if_satisfies(node, constraint, domain)
-			if self.domains[constraint][0] in self.domains[node]:
-				self.domains[node].remove(self.domains[constraint][0])
-				return True
-		return False
+		return revised
 
 	def rerun(self, domains = {}, focal_node = None, constraints = None):
 		'''
@@ -47,7 +49,7 @@ class GAC:
 		self.revise_queue = list()
 		self.domains = domains
 		self.constraints = constraints
-		for constraint in self.constraints.involved[focal_node]:
-			self.revise_queue.append([constraint, focal_node])
+		for constraint_node in self.constraints.involved[focal_node]:
+			self.revise_queue.append([constraint_node, focal_node])
 		self.domain_filtering_loop()
 		return self.domains
